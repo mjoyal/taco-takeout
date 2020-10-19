9476e5ac769ffd308c82b6c8abd844429d496591
ebussiere <eddy@embtech.ca>
2020-10-19 00:38:47 +0000

    stuff

--- helperfunctions/menuItemFormatter.js
+++ helperfunctions/menuItemFormatter.js
@@ -4,7 +4,6 @@ const formatMenuItems = (data) => {
     let priceInt = data[item].price;
     let priceDollars = (priceInt / 100).toFixed(2);
     data[item].price = `$${priceDollars}`;
-
     let imageFIle = data[item].image_url;
     console.log(`images/${imageFIle}`);
     //data[item].image_url = `images/${imageFIle}`;
