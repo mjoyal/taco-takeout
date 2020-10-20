db0acd79afff829521a09f14517dbe9f71be2617
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-10-20 00:41:29 +0000

    added order time to the order table, added info to the database

--- routes/admin.js
+++ routes/admin.js
@@ -2,4 +2,9 @@ module.exports = (router /*, helpers*/) => {
   router.get('/', (req, res) => {
     res.render('admin');
   });
+  router.post('/', (req, res) => {
+    const minutes = req.body.waitTime;
+    console.log(minutes); 
+    const templateVars = {time: minutes}
+  });
 };
