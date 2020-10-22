c0592f8db68bafbd78dcb1f76bd2910fe256dd9a
ebussiere <eddy@embtech.ca>
2020-10-22 11:52:57 +0000

    order_sent

--- routes/checkout.js
+++ routes/checkout.js
@@ -4,61 +4,46 @@ const orderHelpers = require('../db/dbHelpers/orderHelpers');
 const orderItemHelpers = require('../db/dbHelpers/orderItemsHelpers');
 const menuItemFormatter = require("../helperfunctions/menuItemFormatterFunctions");
 const { app } = require("../server");
+
 module.exports = (router /*, helpers*/, db) => {
-  router.get('/:id', (req, res) => {
-    const order_id = req.params.id;
-    return db.query(`
-    SELECT orders.id as order_id, menu_items.name, menu_items.price, order_menu_items.quantity
-    FROM orders
-    JOIN order_menu_items ON orders.id = order_id
-    JOIN menu_items ON menu_item_id = menu_items.id
-    WHERE order_started_at IS NULL;
-    `, [order_id])
-      .then((data) => {
-        res.render('checkout', data.rows[0]);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+  const user_id = 1;
+  router.get('/', (req, res) => {
+    let templateVars = {};
+    orderItemHelpers.getUnplacedOrder(user_id).then((data) => {
+      const menuItems = menuItemHelpers.getAllMenuItems(db);
+      const currentUser = userHelpers.getUserById(user_id, db);
+      const currentCartItems = orderHelpers.getUserCart(user_id, db);
+      Promise.all([menuItems, currentUser, currentCartItems]).then(values => {
+        return values;
+      }).then(values => {
+        values[0] = menuItemFormatter.formatMenuItems(values[0]);
+        return values;
+      }).then(values => {
+        values[2] = menuItemFormatter.formatCartItems(values[2]);
+        return values;
+      }).then(values => {
+        // sets visibility of empty cart
+        let cartHasItems = 0;
+        //calculate and format total for cart contents an pass as a templatevar
+        const cartTotal = menuItemFormatter.calculateCartTotal(values);
+        if (values[2].length) {
+          cartHasItems = 1;
+        }
+        templateVars = {
+          menu_items: values[0],
+          user: values[1],
+          currentCartItems: values[2],
+          cartHasItems: cartHasItems,
+          cartTotal: cartTotal
+        };
+        //console.log(templateVars);
+        return values;
+      }).then(values => {
+        res.render('checkout', templateVars);
+        return values;
+      }).catch(e => {
+        res.send(e);
       });
+    });
   });
 };
-
-
-
-// module.exports = (router /*, helpers*/, db) => {
-
-//   router.get('/', (req, res) => {
-//     let templateVars = {};
-//     const user_id = 1;
-//     const currentCartItems = orderHelpers.getUserCart(user_id, db)
-//       .then((data) => {
-//         //console.log(data);
-//         //console.log(currentCartItems);
-//         return values;
-//       }).then(values => {
-//         values = menuItemFormatter.formatCartItems(values[2]);
-//         return values;
-//       }).then(values => {
-//         const cartTotal = menuItemFormatter.calculateCartTotal(values);
-//         return values;
-//       }).then(values => {
-//         templateVars = {
-//           menu_items: values[0],
-//           user: values[1],
-//           currentCartItems: values[2],
-//           cartHasItems: cartHasItems,
-//           cartTotal: cartTotal
-//         };
-//         return values;
-//       }).then(values => {
-//         console.log(templateVars);
-//         res.render('checkout', templateVars);
-//         return values;
-//       }
-//       ).catch(e => {
-//         res.send(e);
-//       });;
-//   });
-// };
